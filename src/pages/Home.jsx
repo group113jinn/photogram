776e2670f77c6f9e32f67e8aadf806f4f8398c70
Eugene <group113jinn@gmail.com>
2020-09-21 16:40:16 +0300

    home page raw

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 export function Home() {
-    return(
-       <section className="main-home">
-         Home
-       </section>
-    )
+  return (
+    <section className="main-home">
+      <article className="signin-container">
+        <h1>
+          Photogram
+        </h1>
+        <div>
+          <input type="text" placeholder="Username" />
+        </div>
+        <div>
+          <input type="text" placeholder="Password" />
+        </div>
+        <div>
+         <button className="login-button">Login</button>
+      </div>
+        <div>
+         <Link to="/feed" ><button className="guest-button">Guest Mode</button></Link>
+        </div>
+
+
+      </article>
+    </section>
+  )
 }
