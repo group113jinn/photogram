67240f7cf1e0b74ee584526e01902c4423e6c6c7
Eugene <group113jinn@gmail.com>
2020-09-21 19:42:47 +0300

    home sign in

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Logo from "../assets/img/home_logo.png"
 
 export function Home() {
   return (
     <section className="main-home">
+      <section className="home-photo-signin">
+      <article className="home-logo">
+      <img src={Logo} alt="logo" /> 
+      </article>
+
+      
+    
       <article className="signin-container">
         <h1>
           Photogram
@@ -22,8 +29,8 @@ export function Home() {
           <Link to="/feed" ><button className="guest-button">Guest Mode</button></Link>
         </div>
 
-
       </article>
+      </section>
     </section>
   )
 }
